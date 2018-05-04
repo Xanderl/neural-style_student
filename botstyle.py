a3602ca015bac7a7a46b103cbe62de2eca91ab34
Xanderl <35769307+Xanderl@users.noreply.github.com>
2018-05-04 12:37:55 -0400

    neural style can now be run through another script that will be transformed into a discord bot

--- botstyle.py
+++ botstyle.py
@@ -1,12 +1,5 @@
 
+import os
 
+os.system("python neural_style.py --content hal9000.png --styles bob.jpg --output phal321.jpg --width 500")
 
-import neural_style
-
-
-
-neural_style.main()
-
-
-
-python neural_style.py --content hal9000.png --styles gpaint.jpg --output phal.jpg --width 500
