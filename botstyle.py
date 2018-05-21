5830ed9b62b9a6d87bf91c71f20822079e16acdf
Xanderl <35769307+Xanderl@users.noreply.github.com>
2018-05-20 21:06:29 -0400

    users can now use neural style through discord

--- botstyle.py
+++ botstyle.py
@@ -1,10 +1,38 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import bot
+import asyncio
 import os
 
+bot = commands.Bot(command_prefix='#')
+client = discord.Client()
 
-img = input("content enter hal9000.png:")
-pic = input("style enter bob.jpg:")
-name = input("name enter name of new file .png:")
+@bot.event
+async def on_ready():
+    print ("ready for use, just gunna add a happy syntax error here")
+    print (bot.user.id)
 
-os.system("python neural_style.py --content " + img +" --styles " + pic +" --output " + name +" --width 500")
+@bot.command(pass_context=True)
+async def meme(ctx):
+    await bot.say("I like memes")
 
 
+
+@bot.group(pass_context=True)
+async def neural(ctx):
+    if ctx.invoked_subcommand is None:
+        await bot.say('Invalid git command passed...')
+
+@neural.command()
+async def style(ncontent: str, styles: str, name: str):
+    await bot.say('neural styel {} {} {}'.format(ncontent, styles, name))
+    os.system("python neural_style.py --content {} --styles {} --output {} --width 500".format(ncontent, styles, name))
+
+#img = input("content enter hal9000.png:")
+#pic = input("style enter bob.jpg:")
+#name = input("name enter name of new file .png:")
+
+#os.system("python neural_style.py --content " + img +" --styles " + pic +" --output " + name +" --width 500")
+
+
+bot.run("NDQ0NTAxMzc3NjU4NjUwNjUz.DeNKPg.hrAZJ3Am8KQVbZQ3CkSNl3DM9Uk")
--- phttp.py
+++ phttp.py
@@ -1,16 +1,44 @@
 import http.client, json
+app_token = "NDI4NjEzOTAzOTY3MzIyMTEy.DeBn-Q.z_BgHph0_UoG8iglTEfpdhknzgI"
+channel_id = "423897351422214146"
+web_hk = "api/webhooks/447095306841227264/M77Jr-7jT4bIwHEToxNOw1HXzBe4q8-ZOdao6QHZw5ExbhYiVpu8fVKyZVz-Bc4UOQWZ"
+latest_timestamp = ""
+
+def query_server():
+    global app_token, channel_id, latest_timestamp
 
 
-conn = http.client.HTTPSConnection("discordapp.com")
-headers = {"authorization": "Bot MzEyNjQzMTMyNzA3NTY5NjY1.C_eEfQ.8DVBHgDEV3BQu98HtsxwHdNFp8w"}
-conn.request("GET", "/api/channels/312640514405171200/messages", "", headers)
-r1 = conn.getresponse()
+    conn = http.client.HTTPSConnection("discordapp.com")
+    headers = {"authorization": "Bot " + app_token }
+    conn.request("GET", "/api/channels/" + channel_id +"/messages", "", headers)
+    r1 = conn.getresponse()
 
-status = r1.reason
-print(status)
+    status = r1.reason
+    print(status)
 
-r = r1.read().decode('utf-8')
-conversation = json.loads(r)
+    r = r1.read()
+    print(r)
+    conversation = json.loads(r.decode('utf-8'))
 
-print(conversation[0]["timestamp"])
+#    print(json.dumps(conversation, indent=4, sort_keys=True))
 
+    i = 0
+    while i < len(conversation) :
+        comment = conversation[i]
+        i += 1
+
+        timestamp = comment["timestamp"]
+
+        if(timestamp < latest_timestamp) :
+            break
+
+        if comment['attachments'] == []:
+            print("no attachments")
+        else:
+            print(comment['attachments'])
+
+    latest_timestamp = conversation[0]["timestamp"]
+
+print(latest_timestamp)
+query_server()
+print(latest_timestamp)
