2d14c48bbd4eb6646cd4594341d935892b4b0a8a
Rob Korobkin <rob.korobkin@gmail.com>
2018-06-01 13:51:57 -0400

    Fixing file transfer.

--- phttp.py
+++ phttp.py
@@ -1,10 +1,10 @@
-import http.client, json, calendar, os, time, base64
+import http.client, json, calendar, os, time, base64, requests
 from datetime import datetime
 
 
-app_token = "NDQ0NTAxMzc3NjU4NjUwNjUz.DfBrog.eg2yOzAb1bfIoYUEX6ETLOe1t3I"
-channel_id = "430462706559090691"
-web_hk = "api/webhooks/444503005740531714/1r7MT6IfPRwplxY3s3AW3PkwqlGgQltOQfZLA-UbBFLNafkrjDdXreKOt8PmogmPUKTo"
+app_token   = "NDQ0NTAxMzc3NjU4NjUwNjUz.DfBrog.eg2yOzAb1bfIoYUEX6ETLOe1t3I"
+channel_id  = "430462706559090691"
+web_hk      = "api/webhooks/444503005740531714/1r7MT6IfPRwplxY3s3AW3PkwqlGgQltOQfZLA-UbBFLNafkrjDdXreKOt8PmogmPUKTo"
 latest_timestamp = ""
 
 def query_server():
@@ -22,7 +22,6 @@ def query_server():
     print(status)
 
     r = r1.read()
-    print(r)
     conversation = json.loads(r.decode('utf-8'))
 
 #    print(json.dumps(conversation, indent=4, sort_keys=True))
@@ -37,7 +36,7 @@ def query_server():
         if(timestamp <= latest_timestamp) :
             break
 
-        print(comment)
+       # print(comment)
 
         if(comment['content'] == 'Go!') :
 
@@ -54,39 +53,34 @@ def query_server():
                 break
 
 
-            conn = http.client.HTTPSConnection("cdn.discordapp.com")
+            # conn = http.client.HTTPSConnection("cdn.discordapp.com")
+            
             current_time_int = str(int(time.mktime(datetime.utcnow().timetuple())))
 
 
             # download style image
             url = style_comment['attachments'][0]['url']
-            img_path = url.split("https://cdn.discordapp.com")[1]
 
             t = url.split("/")
-            style_img_filename = current_time_int + "-" + t[-1]
+            style_img_filename = "style-" + current_time_int + "-" + t[-1]
 
-            conn.request("GET", img_path, "", headers)
-            r1 = conn.getresponse().read()
+            r = requests.get(url, allow_redirects=True)
+            open(style_img_filename, 'wb').write(r.content)
 
-            style_file = open(style_img_filename, "wb")
-            style_file.write(base64.encodebytes(r1))
-            style_file.close()
-            os.chmod(style_img_filename, 0o777)
+           
+
+
 
             # download content image
             url = content_comment['attachments'][0]['url']
-            img_path = url.split("https://cdn.discordapp.com")[1]
 
             t = url.split("/")
-            content_img_filename = current_time_int + "-" + t[-1]
+            content_img_filename = "content-" + current_time_int + "-" + t[-1]
 
-            conn.request("GET", img_path, "", headers)
-            r1 = conn.getresponse().read()
+            r = requests.get(url, allow_redirects=True)
+            open(content_img_filename, 'wb').write(r.content)
 
-            content_file = open(content_img_filename, "wb")
-            content_file.write(base64.encodebytes(r1))
-            content_file.close()
-            os.chmod(content_img_filename, 0o777)
+
 
             output_img_filename = current_time_int + "-output.jpg"
 
@@ -97,7 +91,5 @@ def query_server():
             os.system(cmd)
 
             break
-
-    print(response)
 
 query_server()
